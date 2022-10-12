d91c80aaea58ec4a24ba28aa5d47ac7f3b93c45a
Gladys-Angela <gladysangelakamau@gmail.com>
2022-10-13 02:07:45 +0300

    Home component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,18 +1,16 @@
 import React from "react";
 //import Navbar from "./Navbar";
-//import Home from "./Home";
+import Home from "./Home";
 //import Landlord from "./Landlord";
 //import NewHouseForm from "./NewHouseForm";
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
 
 function App(){
     return(
         <div className="App">
             <h1>Circle Point Homes</h1>
             <Routes>
-                <Route></Route>
-                <Route></Route>
-                <Route></Route>
+            <Route path="/" element={<Home />} />
             </Routes>
             </div>
     )
