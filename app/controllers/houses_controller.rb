26c943fa2cd73b46d41e5d8cd57058776890da3f
gitahievans <evanygk@gmail.com>
2022-10-12 19:16:38 +0300

    added update and delete actions in houses controller

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,5 +1,6 @@
 class HousesController < ApplicationController
 rescue_from ActiveRecord::RecordInvalid, with: :render_record_invalid
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 wrap_parameters format: []    
 
     def index 
@@ -9,13 +10,34 @@ wrap_parameters format: []
 
     def create 
         house = House.create!(house_params)
-        render json: house, status: :created
+        if house 
+            render json: house, status: :created
+        end
     end
 
+    def update 
+        house = House.find(params[:id])
+        if house 
+            house.update!(house_params)
+            render json: house, status: :updated
+        end
+    end
+
+    def destroy 
+        house = House.find(params[:id])
+        if house
+            house.destroy
+            head :no_content
+    end
+    
     private 
 
     def house_params 
         params.permit(:location, :description, :tenant_id, :landlord_id, :image)
+    end
+
+    def record_not_found 
+        render json: {error: "House not found"}, status: :not_found
     end
 
     def render_record_invalid 
