7743d5245d5521dac3f249e1d4abb84172b0a90a
Gladys-Angela <gladysangelakamau@gmail.com>
2022-10-13 01:24:57 +0300

    first commit

---
+++ client/src/components/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+//import Navbar from "./Navbar";
+//import Home from "./Home";
+//import Landlord from "./Landlord";
+//import NewHouseForm from "./NewHouseForm";
+import {Routes, Route} from "react-router-dom";
+
+function App(){
+    return(
+        <div className="App">
+            <h1>Circle Point Homes</h1>
+            <Routes>
+                <Route></Route>
+                <Route></Route>
+                <Route></Route>
+            </Routes>
+            </div>
+    )
+}
+ export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+//import './components/index.css';
+import App from './components/App.js';
+import { BrowserRouter } from "react-router-dom"
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
+  <BrowserRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
