b29f475d5e63ed982262a0cbb370ce35a1535165
gitahievans <evanygk@gmail.com>
2022-10-12 19:09:44 +0300

    Added index and create actions in houses controller

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,2 +1,24 @@
 class HousesController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :render_record_invalid
+wrap_parameters format: []    
+
+    def index 
+        house = House.all
+        render json: house
+    end
+
+    def create 
+        house = House.create!(house_params)
+        render json: house, status: :created
+    end
+
+    private 
+
+    def house_params 
+        params.permit(:location, :description, :tenant_id, :landlord_id, :image)
+    end
+
+    def render_record_invalid 
+        render json: {error: "Validation Error"}, status: :unprocessable_entity
+    end
 end
