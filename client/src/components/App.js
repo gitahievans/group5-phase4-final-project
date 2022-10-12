281ab1aec037214acb3fe9e52175b02e5f01f7e3
bonveeee <bonochieng@gmail.com>
2022-10-13 01:09:05 +0300

    [added] Navbar component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import Navbar from './Navbar'
+import { BrowserRouter as Router, Routes } from "react-router-dom";
+
 
 function App() {
-    
+
   return (
-    <div>App</div>
+    <Router>
+    <div>
+      <Navbar/>
+    </div>
+
+
+    <Routes>
+        {/* <Route exact path="/" element={<SignupForm />}></Route> */}
+       
+      </Routes>
+    </Router>
   )
 }
 
